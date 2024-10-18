52941c0c624bd4b1e09d15172a4b5c54c9aabaf0
Yosef-ft <yosefetene02@gmail.com>
2024-10-18 07:26:51 +0300

    Script to show the information about dataframes

--- scripts/Utils.py
+++ scripts/Utils.py
@@ -8,6 +8,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+ANSI_ESC = {
+    "PURPLE": "\033[95m",
+    "BLUE": "\033[94m",
+    "GREEN": "\033[92m",
+    "RED": "\033[91m",
+    "ENDC": "\033[0m",
+    "BOLD": "\033[1m",
+    "ITALICS" :"\033[3m"
+}
+
 class DataUtils:
     def __init__(self):
         self.logger = self.setup_logger()
@@ -79,3 +89,78 @@ class DataUtils:
         except Exception as e:
             self.logger.error(f"Error loading data: {e}")
             return None
+        
+
+    def data_info(self, data) -> pd.DataFrame:
+        '''
+        Provides detailed information about the data, including:
+        - Percentage of missing values per column
+        - Number of missing values per column
+        - Data types of the columns
+
+        It also highlights:
+        - The total number of rows and columns in the dataset
+        - Columns with the most missing values
+        - Columns with more than 50% missing values
+
+        Parameter:
+        ----------
+            data(pd.DataFrame): The dataset 
+        
+        Return:
+        -------
+            info_df(pd.DataFrame)
+        '''
+        
+        missing_values = data.isna().sum()
+        missing_percent = round(data.isna().mean() * 100, 2)
+        data_types = data.dtypes
+        
+        info_df = pd.DataFrame({
+            "Missing Values": missing_values,
+            "Missing Percentage": missing_percent,
+            "Data Types": data_types
+        })
+
+
+        info_df = info_df[missing_percent > 0]
+        info_df = info_df.sort_values(by='Missing Percentage', ascending=False)
+
+        max_na_col = list(info_df.loc[info_df['Missing Values'] == info_df['Missing Values'].max()].index)
+        more_than_half_na = list(info_df.loc[info_df['Missing Percentage'] > 50].index)
+        
+
+        print(f"\n{ANSI_ESC['BOLD']}Dataset Overview{ANSI_ESC['ENDC']}")
+        print(f"---------------------")
+        print(f"- {ANSI_ESC['ITALICS']}Total rows{ANSI_ESC['ENDC']}: {data.shape[0]}")
+        print(f"- {ANSI_ESC['ITALICS']}Total columns{ANSI_ESC['ENDC']}: {data.shape[1]}\n")
+
+        duplicated_rows = int(data.duplicated().sum())
+        if duplicated_rows == 0:
+            print(f"{ANSI_ESC['GREEN']}No Duplicated data found in the dataset.{ANSI_ESC['ENDC']}\n")
+        else:
+             print(f"- {ANSI_ESC['RED']}Number of duplicated rows are{ANSI_ESC['ENDC']}: {duplicated_rows}")
+             print(f"- {ANSI_ESC['RED']}Percentage of duplicated rows are{ANSI_ESC['ENDC']}: {round((duplicated_rows / data.shape[0]) * 100, 2)} %\n\n")
+        
+        if info_df.shape[0] > 0:
+            print(f"{ANSI_ESC['BOLD']}Missing Data Summary{ANSI_ESC['ENDC']}")
+            print(f"------------------------")
+            print(f"- {ANSI_ESC['ITALICS']}Columns with missing values{ANSI_ESC['ENDC']}: {info_df.shape[0]}\n")
+            
+            print(f"- {ANSI_ESC['ITALICS']}Column(s) with the most missing values{ANSI_ESC['ENDC']}: `{', '.join(max_na_col)}`")
+            print(f"- {ANSI_ESC['RED']}Number of columns with more than 50% missing values{ANSI_ESC['ENDC']}: `{len(more_than_half_na)}`\n")
+
+
+            if more_than_half_na:
+                print(f"{ANSI_ESC['BOLD']}Columns with more than 50% missing values:{ANSI_ESC['ENDC']}")
+                for column in more_than_half_na:
+                    print(f"   - `{column}`")
+            else:
+                print(f"{ANSI_ESC['GREEN']}No columns with more than 50% missing values.{ANSI_ESC['ENDC']}")
+        else:
+            print(f"{ANSI_ESC['GREEN']}No missing data found in the dataset.{ANSI_ESC['ENDC']}")
+
+        print(f"\n{ANSI_ESC['BOLD']}Detailed Missing Data Information{ANSI_ESC['ENDC']}")
+        print(info_df)
+
+        return info_df
