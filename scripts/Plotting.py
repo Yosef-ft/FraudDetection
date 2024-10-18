9e0dc652582bfe3c2bfc8517b6950768693fb0de
Yosef-ft <yosefetene02@gmail.com>
2024-10-18 08:09:01 +0300

    script for plotting numberical univariant data

--- scripts/Plotting.py
+++ scripts/Plotting.py
@@ -30,6 +30,28 @@ class Plots:
 
         plt.tight_layout()
         plt.show()
+        
 
+    def num_univariant_visualization(self, data: pd.DataFrame):
+        '''
+        This funcions helps in visualizing histograms for numeric columns
+        '''
+
+        numerical_cols = data.select_dtypes(include=['float64', 'int64'])
+        num_cols = len(numerical_cols)
+        nrows = num_cols // 5 + num_cols % 5
+
+        fig, axes = plt.subplots(nrows=nrows, ncols=5, figsize=(20,12))
+        axes = axes.flatten()
+
+        for i, col in enumerate(numerical_cols):
+            sns.histplot(data[col], palette='pastel', ax=axes[i], kde=True)
+            axes[i].set_title(col)
+
+        for j in range(i+1, len(num_cols)):
+            axes[j].set_visible(False)
+
+        plt.tight_layout()
+        plt.show()
 
 
