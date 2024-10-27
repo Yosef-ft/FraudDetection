64f3a1b3824d0b058db107659465ddd17882d69a
Yosef-ft <yosefetene02@gmail.com>
2024-10-27 15:44:42 +0300

    Added script for logging here from __init__.py

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,12 +1,23 @@
-from flask_app import app
+from flask import Flask
 from flask import request
 import pandas as pd
 from .serve_model import predict
-from .utils import Utils
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+app = Flask(__name__)
+
+file_handler = RotatingFileHandler('app.log', maxBytes=10240, backupCount=10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+file_handler.setLevel(logging.DEBUG)
+
+app.logger.addHandler(file_handler)
+
 
 @app.post('/predict')
 def prediction():
-    
+
     data = pd.DataFrame({
         'purchase_value' : [float(request.form['purchase_value'])],
         'age' : [int(request.form['age'])],
@@ -19,6 +30,13 @@ def prediction():
         'country' : [request.form['country']],
     })
 
+    app.logger.debug("Got the required data from user.")
 
     prediction = predict(data)[0]
-    return str(prediction)
+    app.logger.info(f"The model successfully predicted with a value of {prediction}")
+    if prediction == 1:
+        result = 'Fraudulent'
+    else:
+        result = 'Non fraudulent'
+    
+    return result
