33fd7cd4eb1f7cbdeb3e36fa604ecc2ede7e297c
Yosef-ft <yosefetene02@gmail.com>
2024-10-27 15:45:20 +0300

    Bug fix: error while importing modules

--- flask_app/serve_model.py
+++ flask_app/serve_model.py
@@ -1,7 +1,7 @@
 import joblib
 from .utils import Utils
 
-model = joblib.load('../mlartifacts/891150804028438362/6394144cb4b54e858375ecacec0d1094/artifacts/Fraud_RF_path/model.pkl')
+model = joblib.load('mlartifacts/891150804028438362/6394144cb4b54e858375ecacec0d1094/artifacts/Fraud_RF_path/model.pkl')
 
 def predict(data):
     utils = Utils()
